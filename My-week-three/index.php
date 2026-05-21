c87a99488322aaa6c36e5f12f095a1709cac4fe4
jesseburke-04 <jesseburke04@gmail.com>
2026-05-21 19:05:58 -0400

    changes

---
+++ My-week-three/index.php
@@ -0,0 +1,13 @@
+<?php
+    /*
+    API Controller:
+    */
+    require_once "config.php";
+    require_once "LessonMovieHandler.php";
+
+    $lessonActivePage = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+    $lessonHandlerInstance = new LessonMovieHandler(TMDB_BASE_URL, TMDB_API_KEY);
+    $lessonMovieRecords = $lessonHandlerInstance->fetchCurrentPopular($lessonActivePage);
+
+    require_once "views/movies.view.php";
+?>
