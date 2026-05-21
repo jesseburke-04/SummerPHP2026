b999f5991d6864dad62978bdacf8aed97cc410b2
jesseburke-04 <jesseburke04@gmail.com>
2026-05-21 17:45:06 -0400

    changes

---
+++ My-week-three/config.php
@@ -0,0 +1,6 @@
+<?php
+    //this file will define our API information
+    define("TMDB_API_KEY", "f8c35aadf4f54d00bab548e1bdd3ea50");
+    //now add a base URL for the TMDB
+    define("TMDB_BASE_URL", "https://api.themoviedb.org/3");
+?>
