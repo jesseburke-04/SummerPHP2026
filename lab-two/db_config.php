79f71d95fc184e8332cf8c4d5b8dbc6a0f93ad04
jesseburke-04 <jesseburke04@gmail.com>
2026-06-04 19:50:37 -0400

    changes

--- lab-two/db_config.php
+++ lab-two/db_config.php
@@ -1,6 +1,6 @@
 <?php
 // db_config.php
-/define the address where the database server lives
+//define the address where the database server lives
     define("DB_HOST", "172.31.22.43");
     //define the name of the database that we will be using
     define("DB_NAME", "Jesse200657285"); //replace with your info
