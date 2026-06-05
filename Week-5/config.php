07abb40ab075d4835d19241d3900a8166ed79945
jesseburke-04 <jesseburke04@gmail.com>
2026-06-04 23:43:51 -0400

    Update config.php

--- Week-5/config.php
+++ Week-5/config.php
@@ -1,10 +1,10 @@
-<?php
-    //define the address where the database server lives
-    define("DB_HOST", "172.31.22.43");
-    //define the name of the database that we will be using
-    define("DB_NAME", "Jesse200657285"); //replace with your info
-    //define the username for your database
-    define("DB_USER", "YourUsername"); //replace with your info
-    //define the password for the user
-    define("DB_PASSWORD", "YourPassword"); //replace with your info
+<?php 
+  // Define the address where the database server lives
+  define("DB_HOST", "172.31.22.43");
+  // Define the name of the database that we will be using
+  define("DB_NAME", "YourDatabaseName"); // replace with your info
+  // Define the username for your Database name
+  define("DB_USER", "YourUsername"); // replace with your info
+  // Define the password for the user
+  define("DB_PASS", "YourPassword"); // replace with your info
 ?>
